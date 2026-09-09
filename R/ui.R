ee598b9d511bb3642fad855d8175d3a7231b75d1
= <diabbluis@gmail.com>
2026-09-09 08:42:06 -0300

    Improve logo code (favicon)

--- R/ui.R
+++ R/ui.R
@@ -5,10 +5,13 @@ source("components/outputs.R")
 source("components/plot.R")
 source("components/css_styles.R")
 
+# --- UPDATED: Added the global browser tab title parameter ---
 ui <- fluidPage(
+  title = "TCG Deck Simulator & Odds Calculator", # <--- Type your exact browser tab title here!
+
   minimal_css(),
 
-  titlePanel("TCG Probability & Deck Simulator"), # Re-themed title text
+  titlePanel("TCG Probability & Deck Simulator"),
 
   sidebarLayout(
     sidebarPanel(
